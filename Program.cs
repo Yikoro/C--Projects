d430273b0a9a84ef037ba1d8266657175387f280
Yikoro <jgibson1237777@gmail.com>
2026-02-21 19:31:06 -0500

    Boolean Logic assignment

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_logic_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int insurAge = 15;
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?(answer true or false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int maxTickets = 3;
+            int numTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            if (yourAge >= insurAge && dui == false && numTickets <= maxTickets)
+            {
+                Console.WriteLine("You're qualified");
+            }
+            else 
+            {
+                Console.WriteLine("You're not qualified");
+            }
+            
+            
+
+
+        }
+    }
+}
