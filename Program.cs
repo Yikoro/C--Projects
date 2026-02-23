01d2ea84644f964c23d34455d492bc8e448d4534
Yikoro <jgibson1237777@gmail.com>
2026-02-22 20:17:23 -0500

    Branching Submission Assignment

--- Program.cs
+++ Program.cs
@@ -4,32 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Boolean_logic_assignment
+namespace Branching_submission_assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your age?");
-            int insurAge = 15;
-            int yourAge = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Have you ever had a DUI?(answer true or false)");
-            bool dui = Convert.ToBoolean(Console.ReadLine());
-            Console.WriteLine("How many speeding tickets do you have?");
-            int maxTickets = 3;
-            int numTickets = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Qualified?");
-            if (yourAge >= insurAge && dui == false && numTickets <= maxTickets)
-            {
-                Console.WriteLine("You're qualified");
-            }
-            else 
-            {
-                Console.WriteLine("You're not qualified");
-            }
-            
-            
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("What is the weight of your package?");
+            int maxPackageWeight = 50;
+            int userPackageWeight = Convert.ToInt32(Console.ReadLine());
 
+            if (userPackageWeight > maxPackageWeight) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+
+            int maxDimensions = 50;
+
+            Console.WriteLine("What is the package width?");
+            int userPackageWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the package height?");
+            int userPackageheight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the package length?");
+            int userPackageLength = Convert.ToInt32(Console.ReadLine());
+
+            int userDimensions = userPackageWidth + userPackageheight + userPackageLength;
+
+            if (userDimensions > maxDimensions) 
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+
+            int totalQuote = userPackageWidth * userPackageheight * userPackageLength * userPackageWeight / 100;
+            Console.WriteLine("Your estimated total for shipping this package is + $" + (totalQuote) + " Thank you!");
 
         }
     }
