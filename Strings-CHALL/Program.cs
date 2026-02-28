f33bfffc54a3fd012978f88fabac6936214135f1
Yikoro <jgibson1237777@gmail.com>
2026-02-28 17:53:07 -0500

    Strings challenge C#

---
+++ Strings-CHALL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_CHALL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Yusuke ";
+            string middleName = "Toru ";
+            string lastName = "Urameshi";
+            string fullName = firstName + middleName + lastName;
+            
+
+            fullName = fullName.ToUpper();
+            Console.WriteLine(fullName);
+
+
+        }
+    }
+}
