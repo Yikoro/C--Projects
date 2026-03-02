6a506323999d65ad7570c504aec57540d7148304
Yikoro <jgibson1237777@gmail.com>
2026-03-01 21:14:49 -0500

    Update to code that was missing a while statement.

--- Console app while-do loop/Program.cs
+++ Console app while-do loop/Program.cs
@@ -14,39 +14,42 @@ namespace Console_app_while_do_loop
             int userInput = Convert.ToInt32(Console.ReadLine());
 
             bool numberOfStates = userInput == 50;
-
-            do
+            while(!numberOfStates)
             {
-                switch (userInput)
+                do
                 {
-                    case 52:
-                        Console.WriteLine("You guessed 52, but it's a misconception. Try again.");
-                        Console.WriteLine("How many states are there in the USA?");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 1:
-                        Console.WriteLine("You guessed 1, maybe that would be true before the USA was founded. Try again.");
-                        Console.WriteLine("How many states are there in the USA?");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 50:
-                        Console.WriteLine("You guessed 50 good job here's a cookie!");
-                        numberOfStates = true;
-                        break;
-                    case 25:
-                        Console.WriteLine("You guessed 25, are you trying to be funny? Try again.");
-                        Console.WriteLine("How many states are there in the USA?");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    default:
-                        Console.WriteLine("Are you even trying? Try again.");
-                        Console.WriteLine("How many states are there in the USA?");
-                        userInput = Convert.ToInt32(Console.ReadLine());
-                        break;
+                    switch (userInput)
+                    {
+                        case 52:
+                            Console.WriteLine("You guessed 52, but it's a misconception. Try again.");
+                            Console.WriteLine("How many states are there in the USA?");
+                            userInput = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 1:
+                            Console.WriteLine("You guessed 1, maybe that would be true before the USA was founded. Try again.");
+                            Console.WriteLine("How many states are there in the USA?");
+                            userInput = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 50:
+                            Console.WriteLine("You guessed 50 good job here's a cookie!");
+                            numberOfStates = true;
+                            break;
+                        case 25:
+                            Console.WriteLine("You guessed 25, are you trying to be funny? Try again.");
+                            Console.WriteLine("How many states are there in the USA?");
+                            userInput = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        default:
+                            Console.WriteLine("Are you even trying? Try again.");
+                            Console.WriteLine("How many states are there in the USA?");
+                            userInput = Convert.ToInt32(Console.ReadLine());
+                            break;
 
+                    }
                 }
+                while (!numberOfStates);
             }
-            while (!numberOfStates);
+            
         }
     }
 }
