f776c48ebc2b2bdc844b4d475143de2f7d59badf
Yikoro <jgibson1237777@gmail.com>
2026-03-01 23:32:26 -0500

    Calling methods Console app assignment.

---
+++ Calling-Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Return number = new Return();
+            Console.WriteLine("What number do you want to do math operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Multiply by 10 = " + number.Multi(userInput));
+            Console.WriteLine("Add 100 = " + number.Add(userInput));
+            Console.WriteLine("Subtract by 1 = " + number.Sub(userInput)); 
+        }
+    }
+}
---
+++ Calling-Methods/Return.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    internal class Return
+    {
+        public int Multi(int number)
+        {
+            return number * 10;
+        }
+
+        public int Add(int number)
+        {
+            return number + 100;
+        }
+        public int Sub(int number)
+        {
+            return number - 1;
+        }
+    }
+}
