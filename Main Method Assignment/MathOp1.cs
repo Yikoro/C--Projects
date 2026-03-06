93a3f71e952d4706ef5e1eb8e4827affc723dc45
Yikoro <jgibson1237777@gmail.com>
2026-03-06 14:24:16 -0500

    Three methods in one class assignment.

---
+++ Main Method Assignment/MathOp1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class MathOp1
+    {
+        //Adding method
+        public int Calc(int number)
+        {
+            int result = number;
+
+            result = result + 100;
+
+            return result;
+        }
+
+        //decimal multiplication method
+        public int Calc(decimal number)
+        {
+            decimal result = number;
+
+            result = result * 8.7m;
+
+            return (int)result;
+        }
+
+        //string converted to integer method then using sub 
+
+        public int Calc(string number)
+        {
+            if (!int.TryParse(number, out int parsed))
+                throw new ArgumentException($"Cannot convert \"{number}\" to an integer.");
+
+            int result = parsed - 1;
+            Console.WriteLine(result);
+            return result;
+
+        }
+        
+    }
+}
---
+++ Main Method Assignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MathOp1 math = new MathOp1();
+
+            //asking for an int then using adding math op
+            Console.WriteLine("Enter a number.");
+            string input = Console.ReadLine();
+            
+
+            if (int.TryParse(input, out int number))
+            {
+                int answer = math.Calc(number);
+                Console.WriteLine(answer);
+            }
+
+            //asking for a using a decimal input then * math op
+            Console.WriteLine("Now enter a decimal number.");
+            string decIput = Console.ReadLine();
+
+            if (decimal.TryParse(decIput, out decimal decNumber))
+            {
+                int decAnswer = math.Calc(decNumber);
+                Console.WriteLine(decAnswer);
+            }
+
+            //asking user for string converting string into int then doing sub math op
+            Console.WriteLine("Enter a number as a string.");
+            string stringInput = Console.ReadLine();
+
+            try
+            {
+                int stringAnswer = math.Calc(stringInput);
+                Console.WriteLine(stringAnswer);
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+    }
+}
