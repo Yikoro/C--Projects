f387195570add3422f8ac443d83332cde017ae2e
Yikoro <jgibson1237777@gmail.com>
2026-03-08 23:39:51 -0400

    Method #3

---
+++ Method Assignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            maff maff101 = new maff();
+
+            maff101.mathIsHard(10, 50);
+
+            maff101.mathIsHard(one: 20, two: 100);
+        }
+    }
+}
---
+++ Method Assignment3/maff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment3
+{
+    public class maff
+    {
+        public void mathIsHard(int one, int two)
+        {
+            int squared = one * one;
+            Console.WriteLine($"The square of {one} is {squared}");
+            Console.WriteLine($"The second integer is {two}");
+        }
+    }
+}
