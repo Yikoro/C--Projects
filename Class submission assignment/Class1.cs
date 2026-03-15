1ff6ccf83dcfd0c1ea1534039a9ebb8e82749ff6
Yikoro <jgibson1237777@gmail.com>
2026-03-14 23:38:55 -0400

    Class submission assignment

---
+++ Class submission assignment/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_submission_assignment
+{
+    public static class Class1
+    {
+        public static void Check(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
+public class maff
+{
+    //void method that outputs an integer and divides by 2
+    public void Division(int number)
+    {
+        int result = number / 2;
+        Console.WriteLine(result);
+    }
+
+    //Method with output parameters
+    public void Division(int number, out int result, out string message)
+    {
+        result = number / 2;
+        message = $"Output result: {number} / 2 = {result}";
+    }
+
+    //Overload method
+    public void Division(double number)
+    {
+        double result = number / 2.0;
+        Console.WriteLine($"{number} (double) divided by 2 = {result}");
+    }
+}
---
+++ Class submission assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_submission_assignment
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate maff class
+            maff math = new maff();
+
+            //ask user to enter a number
+            Console.WriteLine("Enter a whole number:");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            //shows result to screen
+            math.Division(userNumber);
+
+            //calls overloaded method
+            Console.WriteLine("\nEnter a decimal number:");
+            double decNumber = double.Parse(Console.ReadLine());
+            math.Division(decNumber);
+
+            //calls method with output para
+            math.Division(userNumber, out int outputResult, out string outputMessage);
+            Console.WriteLine("\n" + outputMessage);
+            
+
+            Class1.Check("This was a success.");
+        }
+    }
+}
