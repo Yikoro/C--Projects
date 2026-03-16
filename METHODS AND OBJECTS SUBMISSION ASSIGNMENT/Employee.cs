56c4357b09ae800a6672169ac6626d8eeb63d3dc
Yikoro <jgibson1237777@gmail.com>
2026-03-15 22:37:05 -0400

    Methods and objects submission assignment

---
+++ METHODS AND OBJECTS SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHODS_AND_OBJECTS_SUBMISSION_ASSIGNMENT
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ METHODS AND OBJECTS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHODS_AND_OBJECTS_SUBMISSION_ASSIGNMENT
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            employee.SayName();
+        }
+    }
+}
