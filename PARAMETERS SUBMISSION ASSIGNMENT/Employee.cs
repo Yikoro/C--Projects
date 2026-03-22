598a9f161e36f584e349975db28c86380d704362
Yikoro <jgibson1237777@gmail.com>
2026-03-22 17:47:23 -0400

    PARAMETERS SUBMISSION ASSIGNMENT

---
+++ PARAMETERS SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARAMETERS_SUBMISSION_ASSIGNMENT
+{
+    public class Employee<T>
+    {
+        public string Name { get; set; }
+        public List<T> Things { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+            Things = new List<T>();
+        }
+    }
+}
---
+++ PARAMETERS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARAMETERS_SUBMISSION_ASSIGNMENT
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> stringEmployee = new Employee<string>("Josh");
+            stringEmployee.Things = new List<string> { "Pc", "Phone", "Water Bottle", "Posted Notes" };
+
+            Employee<int> intEmployee = new Employee<int>("Kelly");
+            intEmployee.Things = new List<int> { 40, 10, 1, 106, 11 };
+
+            Console.WriteLine($"(-{stringEmployee.Name}'s Things-)");
+            foreach (string thing in stringEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.WriteLine($"\n(-{intEmployee.Name}'s Things-)");
+            foreach (int thing in intEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
