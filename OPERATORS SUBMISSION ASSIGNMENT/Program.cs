b105bc3ee9d0afbd55681575414fba7f788ae012
Yikoro <jgibson1237777@gmail.com>
2026-03-22 15:31:15 -0400

    Operations submission assignment

---
+++ OPERATORS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    // Overload == operator
+    public static bool operator ==(Employee emp1, Employee emp2)
+    {
+        //null cases
+        if (ReferenceEquals(emp1, emp2)) return true;
+        if (emp1 is null || emp2 is null) return false;
+
+        return emp1.Id == emp2.Id;
+    }
+
+    // != operator (required when overloading ==)
+    public static bool operator !=(Employee emp1, Employee emp2)
+    {
+        return !(emp1 == emp2);
+    }
+
+    // Override Equals() (best practice when overloading ==)
+    public override bool Equals(object obj)
+    {
+        return this == obj as Employee;
+    }
+
+    // Override GetHashCode() (required when overriding Equals)
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee e1 = new Employee(1, "Josh");
+        Employee e2 = new Employee(1, "Sylphy");
+        Employee e3 = new Employee(2, "Chinatsu");
+
+        Console.WriteLine(e1 == e2);
+        Console.WriteLine(e1 == e3);
+        Console.WriteLine(e1 != e3);
+        Console.WriteLine(e1 == null);
+    }
+}
+
+
+
---
+++ OPERATORS SUBMISSION ASSIGNMENT/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OPERATORS SUBMISSION ASSIGNMENT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OPERATORS SUBMISSION ASSIGNMENT")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de163be0-06ea-4a5b-8e38-ef8a5771e3d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
