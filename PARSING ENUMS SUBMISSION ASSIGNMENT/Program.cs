d200b243862eb0249dcf133e65207b990ac93df7
Yikoro <jgibson1237777@gmail.com>
2026-03-22 20:23:09 -0400

    PARSING ENUMS SUBMISSION ASSIGNMENT

---
+++ PARSING ENUMS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARSING_ENUMS_SUBMISSION_ASSIGNMENT
+{
+    enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What day of the week is it today?");
+            string input = Console.ReadLine();
+
+            try
+            {
+                DayOfWeek today = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), input, ignoreCase: true);
+                Console.WriteLine($"Today is: {today}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
