90c6edaae1cca36baa31a0d1da1e50a8289a111d
Yikoro <jgibson1237777@gmail.com>
2026-05-03 20:20:29 -0400

    DATETIME SUBMISSION ASSIGNMENT

---
+++ DATETIME SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATETIME_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Current date and time is...{now}");
+
+            Console.Write("\nEnter a number:");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double hours))
+            {
+                // Step 3: Print the future date and time
+                DateTime future = now.AddHours(hours);
+                Console.WriteLine($"\nIn {hours} hour(s), it will be: {future}");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
---
+++ DATETIME SUBMISSION ASSIGNMENT/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DATETIME SUBMISSION ASSIGNMENT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DATETIME SUBMISSION ASSIGNMENT")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("671f88ad-b2e1-4377-b976-d9a9b2bb0930")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
